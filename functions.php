dfd29e49cf1bd6d830e9772571a663c848de1b95
Dmitry Duplyka <pshechko@outlook.com>
2017-06-21 10:55:57 +0300

    Some code review

--- functions.php
+++ functions.php
@@ -1,35 +1,37 @@
 <?php
 /* Add this code to your functions.php or plugin file */
 
-
-add_action('admin_notices', function () {
+function gf_cache_exists() {
     global $wpdb;
     $wpdb->get_results("SELECT * FROM {$wpdb->prefix}options WHERE option_name LIKE '%_GFCache_%'");
-    if (!$wpdb->num_rows) {
+    return $wpdb->num_rows > 0;
+}
+
+function gf_cache_remove() {
+    global $wpdb;
+    return $wpdb->query("DELETE FROM {$wpdb->prefix}options WHERE option_name LIKE '%_GFCache_%'");
+}
+
+add_action('admin_notices', function () {
+    if (!gf_cache_exists())
         return;
-    }
     ?>
     <div class="notice-warning notice">
         <form method="get">
             <input type="hidden" name="cleargfcache" value="true">
-            In case if Gravity Forms are locked: 
-            <input type="submit" value="Flush Gravityforms Cache" >
+            In case if Gravity Forms are locked 
+            <input class="button button-primary button-large" type="submit" value="Flush Gravityforms Cache" >
         </form>
     </div>
     <?php
 });
 
 add_action('admin_init', function() {
-
-    global $wpdb;
-    $wpdb->get_results("SELECT * FROM {$wpdb->prefix}options WHERE option_name LIKE '%_GFCache_%'");
-    if (!$wpdb->num_rows) {
+    if (!gf_cache_exists())
         return;
-    }
     if (isset($_GET['cleargfcache']) && $_GET['cleargfcache'] === "true") {
         add_action('admin_notices', function () {
-			global $wpdb;
-           $result = $wpdb->query("DELETE FROM {$wpdb->prefix}options WHERE option_name LIKE '%_GFCache_%'");
+            $result = gf_cache_remove();
             ?>
             <div class="notice <?php echo $result ? "updated" : "notice-warning"; ?>">
                 <form method="get">
@@ -37,6 +39,6 @@ add_action('admin_init', function() {
                 </form>
             </div>
             <?php
-        });
+        }, 1);
     }
 });
